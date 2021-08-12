4ac540bf72f2e88520220de389a41ebb1cde2b7e
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-08-13 00:23:09 +0200

    Test

--- variables.py
+++
@@ -1,9 +0,0 @@
-# Your authorization token for Shortform
-authToken = ''
-
-# The file directory of this script; Ex: /Users/nic/python/shortform-to-readwise
-filePath = ''
-
-# Your Readwise API token
-# Retrieve this token from https://readwise.io/access_token
-readwiseToken = ''
