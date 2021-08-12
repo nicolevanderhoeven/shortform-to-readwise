063eee60e525b25c7a35880baa12d60aa5c21a79
nicolevanderhoeven <36070553+nicolevanderhoeven@users.noreply.github.com>
2021-08-13 00:27:45 +0200

    Added back variables.py

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Your authorization token for Shortform
+authToken = ''
+
+# The file directory of this script; Ex: /Users/you/python/shortform-to-readwise
+filePath = ''
+
+# Your Readwise API token
+# Retrieve this token from https://readwise.io/access_token
+readwiseToken = ''
