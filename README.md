4d072c1ed17813557a91044f90c69cbe92136b21
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-08-14 00:28:08 +0200

    Fleshed out readme and cleaned up logging code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# shortform-to-readwise
-This script scrapes Shortform, generates a CSV, and sends the CSV to Readwise.
+# Shortform to Readwise
+
+Work in progress!
+
+This script scrapes [Shortform](https://www.shortform.com) for highlights and sends them to [Readwise](https://readwise.io). Accounts with both Shortform and Readwise are required.
+
+## Usage
+
+Enter the required data in `variables.py`.
+
+`authToken` is the Authorization token that you use for Shortform. Shortform unfortunately doesn't expose this, but you can find out yours by opening up DevTools in Chrome or Firefox and navigating to, for example, [your highlights page](https://www.shortform.com/app/highlights). In the DevTools Network tab, look for the request with the name `?sort=date` and click on it. In the Request Headers panel, you'll see a header `Authorization`, with the value `Basic <your token>`. Copy everything after `Basic ` and paste it into `variables.py`. _(Note: There shouldn't be any spaces in the token.)_
+
+`readwiseToken` is your API token for Readwise, which you can get [here](https://readwise.io/access_token)
+
+Clone or download this repo, cd into it, and run the main script: `python3 gethighlights.py`
+
+You should be able to see your highlights appear in [your Readwise library](https://readwise.io/books).
+
+To run this script automatically, you can use crontab or similar.
--- gethighlights.py
+++ gethighlights.py
@@ -6,38 +6,25 @@ import calendar
 import time
 import json
 
-from variables import authToken, filePath, readwiseToken
+from variables import authToken, readwiseToken
 
 readwiseUrl = 'https://readwise.io/api/v2/highlights/'
-
-
-with open(filePath + '/highlights.csv', 'w') as output_file:
-    output_file.write('Highlight,Title,Author,URL,Note,Location,"Date"')
-
-# Get highlights
-
-response = requests.get('https://www.shortform.com/api/highlights/?sort=date', headers={"Authorization": "Basic " + authToken})
-# print(response.content)
-
-# Parse JSON body
-
 dict = {}
 dict['highlights'] = []
 contents = []
 dictData = {}
 
+# Get highlights
+response = requests.get('https://www.shortform.com/api/highlights/?sort=date', headers={"Authorization": "Basic " + authToken})
+
+# Parse JSON body
 obj = json.loads(response.content)
 for item in obj: # data
     for key in obj[item]: # unnamed full highlight obj
         dictData = {}
-        # value = item[key]
-        # print(key + ': ' + value)
-        # print(key)
         for prop in key: # content, created, id, quote, text...
-            # print('prop = ' +  prop)
             if prop == 'content':
                 for param in key[prop]: # content_type, doc, id, order, title, url_slug
-                    # print(param)
                     if param == 'doc':
                         for meta in key[prop][param]: # author, cover_image, doc_type, id, title, url_slug
                             value = key[prop][param][meta]
@@ -45,7 +32,6 @@ for item in obj: # data
                                 dictData['author'] = value
                             if meta == 'cover_image':
                                 dictData['imageUrl'] = 'https:' + value.replace('\\','')
-                                # print(dict['imageUrl'])
                             if meta == 'title':
                                 dictData['title'] = 'Shortform-' + value
                             if meta == 'url_slug':
@@ -60,55 +46,13 @@ for item in obj: # data
                 value = key[prop]
                 if value != '':
                     dictData['note'] = value
-
-        # Create CSV for logging
-        # with open(filePath + '/highlights.csv', 'a') as output_file:
-        #     output_file.write('\n"' + dict['highlight'] + '",' + dict['title'] + ',' + dict['author'] + ',"' + dict['url'] + '","' + dict['note'] + '","","' + dict['date'] + '"')
-
-        # Add highlights data to dictionary
-
-        # contents.append(dictData)
         if dictData != {}:
             dictData['source_type'] = 'book'
             dict['highlights'].append(dictData)
 
-
-        # Send data to Readwise
-        # body = {
-        #     'highlights': [
-        #         {
-        #             'text': dict['highlight'],
-        #             'title': dict['title'],
-        #             'author': dict['author'],
-        #             'image_url': dict['imageUrl'],
-        #             'source_url': dict['url'],
-        #             'source_type': 'book',
-        #             'note': dict['note'],
-        #             'highlighted_at': dict['date']
-        #         },
-        #     ],
-        # }
-        # response = requests.post(
-        #     url=readwiseUrl,
-        #     headers={"Authorization": "Token " + readwiseToken},
-        #     json={
-        #         'highlights': [{
-        #                 'text': dict['highlight'],
-        #                 'title': dict['title'],
-        #                 'author': dict['author'],
-        #                 'image_url': dict['imageUrl'],
-        #                 'source_url': dict['url'],
-        #                 'source_type': 'book',
-        #                 'highlighted_at': dict['date'],
-        #         }]
-        #     }
-        # )
-
-# print(json.dumps(dict))
-
+# Send highlights to Readwise
 response = requests.post(
     url=readwiseUrl,
     headers={"Authorization": "Token " + readwiseToken},
     json=dict
 )
-print(response.content)
